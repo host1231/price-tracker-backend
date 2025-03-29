aa3ed5dfa736bcefb3d9eb48aca5afd1fd98d2cc
Faik <pg15182100@gmail.com>
2025-03-29 12:59:31 +0400

    First version update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_DB);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('DB OK!');
     } catch (error) {
         console.error('DB error: ', error);
